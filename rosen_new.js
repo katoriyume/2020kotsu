43ba435f3335713a671be8d6e12bd1887df0d7e5
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2020-12-26 03:04:59 +0900

    最低限の機能を実装

--- rosen_new.js
+++ rosen_new.js
@@ -25,12 +25,19 @@ class Boarding {
     return new Boarding( this.arrayed_hash.filter( data => ( lng - this.range < data.lng ) && ( data.lng < lng + this.range ) ) );
   }
   /**
-   * this_arrayed_hashから，重複しない形で路線名を取り出す
+   * this.arrayed_hashから，重複しない形で路線名を取り出す
    * @return {String[]} 路線名記号の配列
    */
   get_lines() {
     return this.arrayed_hash.map( data => data.line ).filter( ( element, index, array ) => array.indexOf(element) == index );
     // arrayed_hash内の駅名のみを取得し，重複を削除
+  }
+  /**
+   * this.arrayed_hashから，パラメータで与えられた路線のデータを取り出す
+   * @param {String} line 路線名コード
+   */
+  line_filter( line ) {
+    return new Boarding( this.arrayed_hash.filter( data => ( data.line == line ) ) )
   }
 
   /**
@@ -54,6 +61,9 @@ class Boarding {
     });
   }
 
+  indexOf( elm ) {
+    return this.arrayed_hash.indexOf( elm );
+  }
   getMin( lat, lng ) {
     let min = (lat - this.hash[0].lat)**2 + (lng - this.hash[0].lng)**2;
     let minObj = hash[0];
@@ -68,12 +78,12 @@ class Boarding {
   getStation( id, direction ) {
     let end_condition;
     if( direction == -1 ) end_condition = 0;
-    else  end_condition = hash.length;
+    else  end_condition = this.arrayed_hash.length;
 
     let now = id;
     while( id != end_condition ) {
-      if( "id" in this.hash[now] ) {
-        return this.hash[now];
+      if( "id" in this.arrayed_hash[now] ) {
+        return this.arrayed_hash[now];
       }
       now += direction;
     }
@@ -87,10 +97,37 @@ class Boarding {
   }
 }
 
+class Display {
+  constructor( screen ) {
+    this.screen = screen;
+    this.nl = new nylon();
+    this.br = new Boarding( dataList );
+    for( let sc of screen ) {
+      this.nl.on( sc, () => {
+        for( let sc2 of screen ) {
+          document.querySelector('#'+sc2).style.display = "none";
+        }
+        document.querySelector('#'+sc).style.display = "block";
+      })
+    }
+    this.nl.on( 'view', (key, params) => {
+      console.log(key);
+      console.log(params);
+      let line = br.line_filter( params.code )
+      let nearest = line.get_points( [params.code], params.lat, params.lng );
+      console.log( nearest );
+      let id = line.indexOf(nearest[0]);
+      console.log( line.getStation( id, params.dir ) );
+      document.querySelector('#next').innerText = line.getStation( id, params.dir ).sta;
+    });
+  }
+}
 let br = new Boarding( dataList );
 
 window.addEventListener('load', () => {
-
+  let display = new Display( ["init", "select", "view"] );
+let nl = new nylon();
+nl.emit( "init" );
 
 console.log("test1");
   // 現在地の設定
@@ -98,6 +135,8 @@ console.log("test1");
 navigator.geolocation.getCurrentPosition(dummy, errorCallback);
 });
 function dummy(position) {
+  let nl = new nylon();
+  nl.emit("select");
   let lat = position.coords.latitude;
   let lng = position.coords.longitude;
   let near = br.lat_filter( lat ).lng_filter( lng ); // 緯度・経度でフィルターをかける
@@ -111,22 +150,33 @@ function dummy(position) {
   let base = document.querySelector('#lines');
   base.innerHTML = '';
   for( let n of nearest ) {
-    let s = document.createElement('span');
-    s.innerText = sta_num[n.line].name;
-    base.appendChild(s);
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    td1.innerText = sta_num[n.line].name;
     let b1 = document.createElement('button');
     b1.setAttribute('type','button');
     b1.setAttribute('value',sta_num[n.line].dir1);
     b1.innerText = sta_num[n.line].dir1;
-    base.appendChild(b1);
+    td2.appendChild(b1);
     let b2 = document.createElement('button');
     b2.setAttribute('type','button');
     b2.setAttribute('value',sta_num[n.line].dir2);
     b2.innerText = sta_num[n.line].dir2;
-    base.appendChild(b2);
-    let br = document.createElement('br');
-    base.appendChild(br);
+    td3.appendChild(b2);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    base.appendChild(tr);
+    b1.addEventListener('click',() => {
+      nl.emit('view', { 'code':n.line, 'dir':-1, 'lat':lat, 'lng':lng } );
+    });
+    b2.addEventListener('click',() => {
+      nl.emit('view', { 'code':n.line, 'dir':1, 'lat':lat, 'lng':lng, 'nearest':n } );
+    });
   }
+  console.log( br.line_filter("JE") );
 }
 /***** ユーザーの現在の位置情報を取得 *****/
 function successCallback(position) {
